bbc25beb3972fbeaaa70c1de527e2f680674996f
Muskan Gupta <muskangzb.19@gmail.com>
2025-05-30 16:37:26 +0530

    Add files via upload

---
+++ tribute_page.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Treibute Page</title>
+    <style>
+        .top{
+            padding-top:16px;
+            padding-bottom:25px;
+            text-align:center;
+            background-color:rgba(27, 10, 138, 0.83);
+            color:white;
+            font-size:20px;
+        }
+        .about{
+       background-color:white;
+       margin:20px;
+        }
+        .img{
+            border-radius:80px;
+            /* padding-top:20px; */
+        }
+        .color{
+            background-color:rgba(162, 164, 165, 0.9);
+        }
+        .achive{
+            padding-top:20px;
+        }
+        .empty{
+           background-color:rgb(162, 164, 165, 0.9);
+           padding:20px;
+        }
+        .foot{
+            text-align:center;
+            color:white;
+            padding-top: 20px;
+            padding-bottom: 20px;
+            font-size:20px;
+            background-color:rgba(27, 10, 138, 0.83);
+        }
+        .head{
+            background-color:rgb(162, 164, 165, 0.9);
+        }
+
+    </style>
+</head>
+<body>
+    <div class="top"> Tribute to A.P.J Abddul Kalam</div>
+    <div class="color">
+    <div class="about">
+        <!-- <a href="images.jpg">About A.P.j Abdul Kalam</a> -->
+         <img class="img" src="images.jpg" alt=" A.P.J Abdul Kalam">
+         <h3>About A.P.J Abdul Kalam</h3>
+         <p>A.P.J. Abdul Kalam (born October 15, 1931, Rameswaram, India—died July 27, 2015, Shillong) was an Indian scientist and politician who played a leading role in the development of India’s missile and nuclear weapons programs. He was president of India from 2002 to 2007. His scientific achievements and popularity gained him the epithets “Missile Man” and “People’s President.”</p>
+    <div class="empty">
+        
+    </div>
+         <div class="achive">
+
+        <h3>
+           Achievements
+        </h3>
+        <ul>
+          <li>India's first Indigenous Hovercraft.</li>
+          <li>India's first Satellite Launch Vehicle.</li>
+           <li>INCOSPAR Committee.</li>
+         <li> Ballistic Missile Project.</li>
+         <li> Chief Scientific Advisor and DRDO Secretary.</li>
+          <li>Launch Vehicle.</li>
+          <li>ntegrated Guided Missile Development Program</li>
+        </ul>
+    </div>
+    </div>
+    <header calss="head">
+
+    </header>
+    <footer class="foot">
+    Created by Muskan Gupta.All rights reserved.
+    </footer>
+    </div>
+</body>
+</html>
